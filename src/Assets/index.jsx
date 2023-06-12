753d609572e7a29e71eb0627c5732b0547ab2517
Alen.m <alenmullassery123@gmail.com>
2023-06-12 18:51:09 +0530

    Desinged the structure of Extra Activity section with a logo and description

--- src/Assets/index.jsx
+++ src/Assets/index.jsx
@@ -1,12 +1,14 @@
 import logo from './png/icons/logo.png'
 import Biography from "./png/background-img/biography.jpg"
 import Service from "./png/icons/service-icon.png"
-
-
-
+import BrototypeLogo from "./png/icons/actvity-logo.png"
+import CompanyLogo from "./png/icons/company-logo.png"
+import ExpoLogo from "./png/icons/expo-icon.png"
 export{
     logo,
     Biography,
     Service,
-
+    BrototypeLogo,
+    CompanyLogo,
+    ExpoLogo,
 };
--- src/Components/About/Skills.jsx
+++ src/Components/About/Skills.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { BrototypeLogo, CompanyLogo, ExpoLogo, } from '../../Assets'
 
 function Skills() {
     return (
@@ -13,31 +14,31 @@ function Skills() {
                             <div>
                                 <h2 className='progress-content'>Web Designing</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"20%"}}></div>
+                                    <div className='progress-value' style={{ width: "64%", left: "62%" }} data-percent="64%"></div>
                                 </div>
-                            </div> 
+                            </div>
                             <div>
                                 <h2 className='progress-content'>Responsive</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"30%"}}></div>
+                                    <div className='progress-value' style={{ width: "40%", left: "38%" }} data-percent="40%"></div>
                                 </div>
                             </div>
                             <div>
                                 <h2 className='progress-content'>Web Designing</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"40%"}}></div>
+                                    <div className='progress-value' style={{ width: "30%", left: "28%" }} data-percent="30%"></div>
                                 </div>
-                            </div> 
+                            </div>
                             <div>
                                 <h2 className='progress-content'>Responsive</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"50%"}}></div>
+                                    <div className='progress-value' style={{ width: "80%", left: "78%" }} data-percent="80%"></div>
                                 </div>
                             </div>
                             <div>
                                 <h2 className='progress-content'>Python</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"60%"}}></div>
+                                    <div className='progress-value' style={{ width: "60%", left: "58%" }} data-percent="60%"></div>
                                 </div>
                             </div>
                         </div>
@@ -46,31 +47,31 @@ function Skills() {
                             <div>
                                 <h2 className='progress-content'>Web Designing</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"70%"}}></div>
+                                    <div className='progress-value' style={{ width: "45%", left: "43%" }} data-percent="45%"></div>
                                 </div>
                             </div>
                             <div>
                                 <h2 className='progress-content'>Web Designing</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"85%"}}></div>
+                                    <div className='progress-value' style={{ width: "89%", left: "87%" }} data-percent="89%"></div>
                                 </div>
                             </div>
                             <div>
                                 <h2 className='progress-content'>Web Designing</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"72%"}}></div>
-                                </div>
-                            </div> 
-                            <div>
-                                <h2 className='progress-content'>Responsive</h2>
-                                <div className="progress">
-                                    <div className='progress-value' style={{width:"98%"}}></div>
+                                    <div className='progress-value' style={{ width: "72%", left: "70%" }} data-percent="72%"></div>
                                 </div>
                             </div>
                             <div>
                                 <h2 className='progress-content'>Responsive</h2>
                                 <div className="progress">
-                                    <div className='progress-value' style={{width:"60%"}}></div>
+                                    <div className='progress-value' style={{ width: "83%", left: "81%" }} data-percent="83%"></div>
+                                </div>
+                            </div>
+                            <div>
+                                <h2 className='progress-content'>Responsive</h2>
+                                <div className="progress">
+                                    <div className='progress-value' style={{ width: "45%", left: "43%" }} data-percent="45%"></div>
                                 </div>
                             </div>
                         </div>
@@ -78,7 +79,41 @@ function Skills() {
 
                 </div>
             </section>
-            
+            <section className='extra-activities'>
+
+                <div className='extra-activity-container'>
+                    <div className='activities'>
+                        <h1>Extra Activities</h1>
+                    </div>
+                    <div className=' extra-act-div'>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={BrototypeLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={CompanyLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={ExpoLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={BrototypeLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={BrototypeLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                        <div className='extra-activity-prop'>
+                            <img className='extra-act-img' src={BrototypeLogo} alt="img" />
+                            <p className='extra-act-des'>Intensive coding bootcamp program by Brototype <br />(March 2022 - present)</p>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </div>
     )
 }
